08275f46c1d9a9f61ce2473e32c97dbed49ad219
MBrennecke <MBrennecke10@gmail.com>
2018-09-18 10:11:05 -0600

    working it

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,22 +8,36 @@ module.exports = function(app) {
   // GET route for getting all of the todos
   app.get("/", function(req, res) {
 	db.Burger.findAll({}).then(function(dbBurger) {
-		res.json(dbBurger);
+		var hbsObject = {
+		burger_name: dbBurger
+		};
+		console.log(hbsObject);
+    res.render("index", hbsObject);
 	});
   });
 
   // POST route for saving a new todo. You can create a todo using the data on req.body
-  app.post("/api/todos", function(req, res) {
-
-  });
-
-  // DELETE route for deleting todos. You can access the todo's id in req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
-
+  app.post("/api/burgers", function(req, res) {
+	 db.Burger.create({
+		 burger_name: req.body.name,
+		 devoured: false
+	  })
+      .then(function(dbBurger) {
+        res.json(dbBurger);
+      });
   });
 
   // PUT route for updating todos. The updated todo will be available in req.body
-  app.put("/api/todos", function(req, res) {
-
+  app.put("/api/burgers/:id", function(req, res) {
+	db.Burger.update(
+	{
+		devoured: req.body.devoured
+	},{
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbBurger) {
+        res.json(dbBurger);
+      });
   });
 };
